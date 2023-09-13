1a78fc5e8c491014eaca5f0d21eef889fd9e61b5
Frédéric Mascaro <frederic.mascaro@gmail.com>
2023-09-13 10:13:53 +0200

    publish types and code in format ESM and CJS

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ import { get_all_containers } from "./commands/get_all_containers"
 import { run_container } from "./commands/run_container"
 import { stop_container } from "./commands/stop_container"
 
+export type { DockerContainer, GetDockerContainerConfig } from "./commands/get_all_containers"
+export type { RunContainerConfig, ContainerEnvironmentVariable, ContainerPort, ContainerVolume } from "./commands/run_container"
+export type { StopContainerConfig } from "./commands/stop_container"
+
 const docker = {
   run: run_container,
   stop: stop_container,
   ps: get_all_containers
 }
+
+export type Docker = typeof docker
 
 export default docker
